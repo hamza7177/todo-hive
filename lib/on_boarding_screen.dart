33f21cff84c711337aee12a7520952f8ba9e3201
Hamza Tariq <100077755+hamza7177@users.noreply.github.com>
2025-02-17 20:09:47 +0500

    fonts

---
+++ lib/on_boarding_screen.dart
@@ -0,0 +1,8 @@
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const bgColor = Color(0xff0F172A);
+  static const primary = Color(0xff0F172A);
+  static const secondary = Color(0xff2E3AA4);
+  static const lightPrimary = Color(0xffD4F5FF);
+  static const lightSecondary = Color(0xffEEEFF8);
+  static const cardColor = Color.fromARGB(31, 30, 159, 240);
+  static const black = Color.fromARGB(255, 17, 17, 17);
+  static const white = Colors.white;
+  static const whiteSplash = Colors.white12;
+  static const primarySplash = Color.fromARGB(118, 96, 126, 166);
+  static const darkRed = Color(0xffFF2D2D);
+  static const grey = Color(0xff838990);
+  static const darkGrey = Color(0xff333333);
+  static const lightGrey = Color.fromARGB(255, 191, 198, 206);
+  static const ultraLightGrey = Color.fromARGB(255, 221, 229, 238);
+  static const red = Color(0xffFFD4D4);
+  static const green = Color(0xffD4FFEA);
+  static const blue = Color(0xffD4F5FF);
+  static const lightOrange = Color(0xffFFE9D4);
+  static const orange = Color(0xffE85F51);
+
+  static const MaterialColor primarySwatch = MaterialColor(
+    0xff0F172A,
+    {
+      50: Color(0xff95d0f5),
+      100: Color(0xff74c2f2),
+      200: Color(0xff5cb8f2),
+      300: Color(0xff3facf2),
+      400: Color(0xff1e9ef0),
+      500: Color(0xff1189d6),
+      600: Color(0xff0d76ba),
+      700: Color(0xff0b68a3),
+      800: Color(0xff085687),
+      900: Color(0xff07466e),
+    },
+  );
+}
